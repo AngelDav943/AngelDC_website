453faf9163bc97bab8f6b69d58b3bc94b35505f8
Angel <35638964+AngelDav943@users.noreply.github.com>
2022-03-07 03:46:36 +0000

    new/item updated

--- view/adminpanel/new/item.js
+++ view/adminpanel/new/item.js
@@ -1,1 +1,30 @@
-// TODO: Create a menu to easily create new items and add them to the database
+
+const accounts = require(`${__dirname}/../../server-modules/accounts.js`);
+const cookies = require(`${__dirname}/../../server-modules/cookies.js`);
+
+// TODO: Connect firestore database to item creation page
+
+accounts.getUserByUID(cookies.getCookie(req.headers.cookie, "uid")).then(user => {
+    if (user && user.perms.admin == true) {
+		console.log("adminpanel/new/item @"+user.name)
+        new page.loader({
+            "res":res,
+            "req":req,
+            "template":fs.readFileSync(`${__dirname}/../../pages/admin/new/item.html`).toString(),
+            "other":{
+				"itempreview": new page.templater({
+					"templatedir": `${__dirname}/../../assets/public/templates/shopitem.html`,
+					"other": {
+						"item": {
+							"name":"",
+							"cost":0
+						},
+						"islimited":""
+					}
+				}).load()
+			}
+        }).load()
+    } else {
+        res.redirect(`${page.url}`)
+    }
+})
